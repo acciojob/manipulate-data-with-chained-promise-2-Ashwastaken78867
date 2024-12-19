1a17b3e47d51fa6dbf52dbaf64590d303fdddcaf
Bagda@Sh <168397146+Ashwastaken78867@users.noreply.github.com>
2024-12-19 10:10:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+function getNumbers() {
+	return new Promise((reslove) => {
+		setTimeout(() => reslove([1,2,3,4]), 3000);
+});	
+}
+
+getNumbers()
+.then((numbers) =>{
+	return new Promise((resolve) =>{
+		const even = numbers.fliter((num) => num%2===0);
+		document.getElementById('output').textContent = "Even Numbers: + evens.join(', ');   "
+	setTimeout(() => resolve(even), 1000);
+	})
+})
+
+.then((evens) =>{
+	const multiplied = evens.map((num) => num*2);
+	setTimeout(() =>{
+		document.getElementById('output').textContent = "Multiplied Numbers:" + multiplied.join(',');
+		
+	}, 2000);
+})
+
